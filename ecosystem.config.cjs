5732acb010bcca97739acab3e5199ace8c0ca17b
Vassil Shterev <vsterev@gmail.com>
2025-04-23 12:20:05 +0300

    Update post-deploy script to use startOrReload for better process management

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -26,9 +26,8 @@ module.exports = {
 			path: '/home/vsterev/git/shbm/pm2/shbm-frontend',
 			repo: 'git@github.com:vsterev/shbm-fe.git',
 			'post-deploy': `yarn \
-		  && pm2 stop shbm-fe || true\
-		  && yarn build --outDir /home/vsterev/git/shbm/static/static_shbm_frontend_1 --mode production \
-		  && pm2 start ecosystem.config.cjs --only shbm-fe`,
+		  && yarn build --outDir /home/vsterev/git/shbm/static/static_shbm_frontend_1 --mode production --emptyOutDir \
+		  && pm2 startOrReload ecosystem.config.cjs --only shbm-fe`,
 		},
 	},
 };
