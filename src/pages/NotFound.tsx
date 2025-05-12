095908fa0f1176058a7dac4bd3f41ef8ece56abe
Vassil Shterev <vsterev@gmail.com>
2025-05-12 08:31:26 +0300

    change background images link

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import { View, Text, Image } from 'reshaped';
 const NotFound = () => {
   return (
     <View height={100} width="100%" justify="center" align="center" gap={5}>
-      <Image src="public/images/not-found.png" height={100} />
+      <Image src="/images/not-found.png" height={100} />
       <Text variant="title-4" color="neutral-faded">
         404 - Page not found
       </Text>
